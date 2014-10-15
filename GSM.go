6d06af2e91d3b03af6b9f81aff1b12716af32d5b
shanshan <ss.orangeorange@gmail.com>
2014-10-15 14:03:49 -0500

    solve some bugs

--- GSM.go
+++ GSM.go
@@ -6,8 +6,9 @@ import (
    "bufio"
    "bytes"
    "sync"
-   "runtime"
+   // "runtime"
    "time"
+   "sort"
    "io/ioutil"
 )
 
@@ -20,7 +21,13 @@ func main() {
     start_time := time.Now()
     gsm := make(map[int]int)
 
-    for j, fi := range files {
+    core_num := 3
+    kmer_len := 5
+    distance := 10
+    // runtime.GOMAXPROCS(core_num+2)
+
+    for index, fi := range files {
+        fmt.Println(fi.Name())
     	f,err := os.Open(os.Args[1] + "/" + fi.Name())
         if err != nil {
             fmt.Printf("%v\n",err)
@@ -31,15 +38,15 @@ func main() {
         br := bufio.NewReader(f)
         byte_array := bytes.Buffer{}
 
-        gname, isPrefix, err := br.ReadLine()
+        _, isPrefix, err := br.ReadLine()
 
         if err != nil || isPrefix {
     		fmt.Printf("%v\n",err)
-    		os.Exit(1)    	
+    		os.Exit(1)
         }
 
-        header := make([]byte, len(gname))
-        copy(header, gname)
+        // header := make([]byte, len(gname))
+        // copy(header, gname)
 
         for {
             line , isPrefix, err := br.ReadLine()
@@ -53,60 +60,71 @@ func main() {
         input := []byte(byte_array.String())
         fmt.Println(len(input))
         var wg sync.WaitGroup
-        core_num := 1
-        kmer_len := 5
-        distance := 10
-        result := make(chan int, 1000000)
-        runtime.GOMAXPROCS(core_num)
-
-        go func() {
-            wg.Wait()
-            close(result)
-        }()
+        result := make(chan int, core_num)
 
         for i := 0; i < core_num; i++ {
             wg.Add(1)
-        	go func(genome []byte, index int, core_num int, kmer_len int, distance int, result chan int) {
-                defer wg.Done()
-        		begin := len(genome)*index/core_num
-        		end := len(genome)*(index+1)/core_num
-                if begin != 0 {
-                    begin = begin - kmer_len
-                }
-        		fmt.Println(begin, end)
-        		for m := begin; m < end-2*kmer_len-distance; m++ {
-                    fmt.Println("m", m)
-    				kmer := append(genome[m:m+kmer_len], genome[m+kmer_len+distance:m+2*kmer_len+distance]...)
-    				repr := 0
-    				for j := 0; j<len(kmer); j++ {
-    					switch kmer[j] {
-    						case 'A': repr = 4*repr
-    						case 'C': repr = 4*repr + 1
-    						case 'G': repr = 4*repr + 2
-    						case 'T': repr = 4*repr + 3
-    						default:
-    						// we skip any qgram that contains a non-standard base, e.g. N
-    						  repr = repr
-    					}
-    				}
-    				result <- repr
-        		}
-        	}(input, i, core_num, kmer_len, distance, result)
+            fmt.Println(i)
+        	go process(input, i, core_num, kmer_len, distance, result, &wg)
         }
+
+        go func() {
+            wg.Wait()
+            fmt.Println("close result")
+            close(result)
+        }()
 
         for res := range result {
             if gsm[res] == 0 {
-                gsm[res] = j+1
-            } else if gsm[res] == j+1 {
-                break
+                gsm[res] = index+1
+            } else if gsm[res] == index+1 {
+
             } else {
                 gsm[res] = -1
             }
         }
     }
+    // fmt.Println(gsm)
+    fmt.Println(len(gsm))
+    var keys []int
+    for k := range gsm {
+        keys = append(keys, k)
+    }
 
-    fmt.Println(gsm)
+    sort.Ints(keys)
+    for _, k := range keys {
+        fmt.Println("Key:", k, "Value:", gsm[k])
+    }
 
     gsm_time := time.Since(start_time)
     fmt.Println("used time", gsm_time)
+}
+
+func process(genome []byte, i int, core_num int, kmer_len int, distance int, result chan int, wg *sync.WaitGroup) {
+    fmt.Println("iN", i)
+    defer wg.Done()
+    fmt.Println("loopin", i)
+    begin := len(genome)*i/core_num
+    end := len(genome)*(i+1)/core_num
+    if begin != 0 {
+        begin = begin - kmer_len
+    }
+    fmt.Println(begin, end)
+    for m := begin; m < end-2*kmer_len-distance; m++ {
+        // fmt.Println("m", m)
+        kmer := append(genome[m:m+kmer_len], genome[m+kmer_len+distance:m+2*kmer_len+distance]...)
+        repr := 0
+        for j := 0; j<len(kmer); j++ {
+            switch kmer[j] {
+                case 'A': repr = 4*repr
+                case 'C': repr = 4*repr + 1
+                case 'G': repr = 4*repr + 2
+                case 'T': repr = 4*repr + 3
+                default:
+                // we skip any qgram that contains a non-standard base, e.g. N
+                  repr = repr
+            }
+        }
+        result <- repr
+    }
 }
