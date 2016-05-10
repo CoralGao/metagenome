7781360f37a2db1f786d73c7bc8b7437316a1194
shanshan <ss.orangeorange@gmail.com>
2016-05-10 15:30:34 -0500

    add randomize round

--- paired_guess_sequence.go
+++ paired_guess_sequence.go
@@ -6,12 +6,15 @@ import (
 	"math/rand"
 	"time"
 	"os"
+	"bytes"
 	"bufio"
+	"runtime"
 )
 
 //-----------------------------------------------------------------------------
 func main() {
 	rand.Seed(time.Now().UnixNano())
+	runtime.GOMAXPROCS( runtime.NumCPU() )
 
 	genome := os.Args[1]
 	read := os.Args[2]
@@ -21,18 +24,17 @@ func main() {
 	r := bufio.NewReader(f)
 	i := 0
 	fmt.Println("guessID")
-	read1 := ""
+	var read1, read2 []byte
 	readsToGenome := make([]int, 50)
 	for {
         line, err := r.ReadBytes('\n')
         if err != nil { break }
-        if len(line) > 1 && i %2 ==1 {
-        	line = line[0:len(line)-1]
-        	if i % 4 == 1 {
-        		read1 = string(line)
-        	} else if i % 4 == 3{
-        		read2 := line
-        		seq := saved_idx.GuessPair([]byte(read1), []byte(read2), 30)
+        if len(line) > 1 {
+        	if i % 8 == 1 {
+        		read1 = bytes.TrimSpace(line)
+        	} else if i % 8 == 5{
+        		read2 = bytes.TrimSpace(line)
+        		seq := saved_idx.GuessPair(read1, reverse_complement(read2), 100, 1500)
         		fmt.Println(seq)
 
         		if seq != -1 {
@@ -49,6 +51,23 @@ func main() {
     }*/
 }
 
+func reverse_complement(s []byte) []byte {
+	rs := make([]byte, len(s))
+	for i:=0; i<len(s); i++ {
+		if s[i] == 'A' {
+			rs[len(s)-i-1] = 'T'
+		} else if s[i] == 'T' {
+			rs[len(s)-i-1] = 'A'
+		} else if s[i] == 'C' {
+			rs[len(s)-i-1] = 'G'
+		} else if s[i] == 'G' {
+			rs[len(s)-i-1] = 'C'
+		} else {
+			rs[len(s)-i-1] = s[i]
+		}
+	}
+	return rs
+}
 func check_for_error(e error) {
 	if e != nil {
 		panic(e)
